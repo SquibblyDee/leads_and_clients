d6f5d0210fac7b8f9af4edd7baa465702f2f835c
unknown <steelst0@gmail.com>
2018-07-09 13:32:12 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+# import the function connectToMySQL from the file mysqlconnection.py
+from mysqlconnection import connectToMySQL
+app = Flask(__name__)
+# invoke the connectToMySQL function and pass it the name of the database we're usingcopy
+# connectToMySQL returns an instance of MySQLConnection, which we will store in the variable 'mysql'
+mysql = connectToMySQL('lead_gen_business')
+# now, we may invoke the query_db method
+
+@app.route('/')
+def index():
+    query = mysql.query_db("SELECT CONCAT(clients.first_name, ' ', clients.last_name) AS client_name, COUNT(*) as count FROM leads JOIN sites ON leads.site_id = sites.site_id JOIN clients ON sites.client_id = clients.client_id GROUP BY client_name ORDER BY count")
+    print("Fetched all the stu", query, "\n")
+    return render_template('index.html', query = query)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/scripts.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+            
+    var options = {
+        animationEnabled: true,
+        title: {
+            text: "Customers and number of new leads"
+        },
+        data: [{
+            type: "doughnut",
+            innerRadius: "40%",
+            showInLegend: true,
+            legendText: "{label}",
+            indexLabel: "{label}: {y}",
+            dataPoints: [
+                { label: "Alvin Malone", y: 2 },
+                { label: "Caesar Stewart", y: 2 },
+                { label: "Francis Walsh", y: 4 },
+                { label: "Roosevelt Sebial", y: 10 },
+                { label: "Erica Suarez", y: 13 },
+                { label: "Callie Poole", y: 13 },
+                { label: "Ferdinand Schmidt", y: 14 },
+                { label: "Alexa Schroeder", y: 16 },
+                { label: "Shafira Hansen", y: 17 },
+                { label: "Maria Gonzales", y: 18 },
+                { label: "Diana Sue Manlulu", y: 20 },
+                { label: "Masaki Fujimoto", y: 21 },
+                { label: "Tom Owen", y: 22 },
+                { label: "Toni Rose Panganiban", y: 30 },
+                { label: "John Supsupin", y: 32 },
+                { label: "Ryan Owen", y: 32 },
+                { label: "Joe Smith", y: 34 },
+                { label: "Michael Choi", y: 35 }
+            ]
+        }]
+    };
+    $("#chartContainer").CanvasJSChart(options);
+    
+    }
